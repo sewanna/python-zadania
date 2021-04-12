12ec4bdfe2c9d44e08390120370a9244b3a10c2a
lena wrotz <seweryniakanna1gmail.com>
2021-04-12 21:03:06 +0200

    .

---
+++ min.py
@@ -0,0 +1,22 @@
+#https://edabit.com/challenge
+# 
+# #Convert Minutes into Seconds
+# #Write a function that takes an integer minutes and converts it to seconds.
+def mintosec(min):
+    sec = 60*min
+    return sec
+t=mintosec(60)    
+print('Liczba sekund w 60 minut= ',t)
+
+# napisz funkcje ktora zwraca x^2 dla x 
+
+
+def kwadrat(x):
+    liczba = x * x
+    return liczba
+print("Kwadrat liczby 2 wynosi: " , kwadrat(2) )
+print("Kwadrat liczby 5 wynosi: " , kwadrat(5) )
+
+# napisz funkcje ktora zwraca x^2 dla x w przedziale [1,3] .,.,. 
+
+
